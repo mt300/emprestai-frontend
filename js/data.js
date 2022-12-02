a45dbe667bd64308b31cbf403885f53d988310c8
mt300 <matheus.tomazi5@gmail.com>
2022-12-02 20:11:30 -0300

    implementing  login and join functionalities

--- js/data.js
+++ js/data.js
@@ -55,14 +55,36 @@ const Data = [
     }
 ];
 
+const Users = [
+    {
+        name: "Jon",
+        login: "user@email.com",
+        password: "123",
+        city: "Rio de Janeiro/RJ"
+    },
+    {
+        name: "Aly",
+        login: "resu@email.com",
+        password: "123",
+        city: "Aracaju/SE"
+    }
+]
+
+const Logged = {
+    value: false,
+    user: "",
+    city: ""
+}
 if( localStorage.getItem("data") == undefined){
     localStorage.setItem("data", JSON.stringify(Data));
     localStorage.setItem("id-counter", Data.length)
     localStorage.setItem("data-demand", JSON.stringify(Data.filter(e=>{return (e.type == "demand")})));
     sessionStorage.setItem("data-demand", JSON.stringify(Data.filter(e=>{return (e.type == "demand")})));
     sessionStorage.setItem("data-offer", JSON.stringify(Data.filter(e=>{return (e.type == "offer")})));
-    
+    sessionStorage.setItem("users",JSON.stringify(Users));
+    sessionStorage.setItem("logged",JSON.stringify(Logged))
 }
+
 
 
  // delete data
@@ -91,29 +113,101 @@ function fetchData(){
         // copying and creating new posts with data stored
         newPost.innerHTML = post.innerHTML;
         newPost.classList.add("post-bar");
-
+        
         var user = newPost.querySelector("#user") 
         user.childNodes[0].data = data.user;
-
+        
         var obj = newPost.querySelector("#obj")
         obj.childNodes[1].data = data.obj
-
+        
         var deadLine = newPost.querySelector("#dead-line")
         deadLine.childNodes[1].data = data.deadLine;
-
-
+        
+        
         var voltage = newPost.querySelector("#voltage")
         voltage.childNodes[1].data = data.voltage;
-
+        
         var desc = newPost.querySelector("#desc")
         desc.childNodes[1].data = data.desc;
-
+        
         newPost.querySelector("#delete-post").addEventListener("click",() => deletePost(data.id));
         
         // console.log(deleteBtn)
         
         postsSection.appendChild(newPost)
     });
+    var user = JSON.parse(sessionStorage.getItem("logged"));
+    if(user.value == true){
+        document.querySelector("#user-name-logged").textContent = user.user;
+        document.querySelector("#city").textContent = user.city;
+    }
+}
+
+function login() {
+    var users = JSON.parse(sessionStorage.getItem("users"));
+    var userLogin = document.querySelector("#user-login").value;
+    var userPassword = document.querySelector("#password-login").value;
+    var name;
+    var city;
+    if(users.find((user) => { 
+        if(userPassword == user.password){
+            // console.log("Access.")    
+            name = user.name;
+            city = user.city;
+            return userLogin == user.login
+        }else{
+            // console.log("invalid password")
+            return false;
+        }
+    })){
+        // window.location.href = "file:///home/tomazi/Documents/getninjas/stupids/EmprestaAi/index.html"
+        // var logged = sessionStorage.getItem("logged");
+        var logged = {
+            value:true,
+            user: name,
+            city: city
+        };
+        console.log(name);
+        sessionStorage.setItem("logged",JSON.stringify(logged));
+        // console.log(location.href)
+    }else{
+        console.log("No")
+    }
+}
+function redirectIndex(){
+    var logged = JSON.parse(sessionStorage.getItem("logged"));
+    console.log(logged.value)
+    if(logged.value == true){
+        location.replace("file:///home/tomazi/Documents/getninjas/stupids/EmprestaAi/index.html");
+        console.log(logged)
+    }   
+    // console.log(logged)
+        
+}
+function logout(){
+    var logged = JSON.parse(sessionStorage.getItem("logged"));
+    logged.value = false;
+    sessionStorage.setItem("logged",JSON.stringify(logged));    
+}
+
+function join(){
+    var name = document.querySelector("#nome").value;
+    var email = document.querySelector("#email").value;
+    var password = document.querySelector("#senha").value;
+    var municipio = document.querySelector("#municipio").value;
+    var estado = document.querySelector("#estado").value;
+    var tc = document.querySelector("#c2").value;
+
+    var user = {
+        name: name,
+        login: email,
+        password: password,
+        city: municipio + "/" + estado
+    }
+    var users = JSON.parse(sessionStorage.getItem("users"));
+    users.push(user);
+    console.log(user)
+    sessionStorage.setItem("users",JSON.stringify(users));
 }
 
 function fetchRequests(){
