f828e92e9f0d440ad33924f986a49bb2bccb8967
Matheus <matheus@8n1.com.br>
2022-12-02 11:59:27 -0300

    add php files back

---
+++ cadastro.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include("conexao.php");
+
+$nome = mysqli_real_escape_string($conexao, trim($_POST['nome']));
+$email = mysqli_real_escape_string($conexao, trim($_POST['email']));
+$municipio = mysqli_real_escape_string($conexao, trim($_POST['municipio']));
+$estado = mysqli_real_escape_string($conexao, trim($_POST['estado']));
+$senha = mysqli_real_escape_string($conexao, trim(md5($_POST['senha'])));
+
+$sql = "select count(*) as total from usuario where usuario = '$email'";
+$result = mysqli_query($conexao, $sql);
+$row = mysqli_fetch_assoc($result);
+
+if($row['total'] == 1) {
+	$_SESSION['usuario_existe'] = true;
+	header('Location: cadastro.php');
+	exit;
+}
+
+$sql = "INSERT INTO usuario (nome, usuario, senha, data_cadastro) VALUES ('$nome', '$email', '$municipio', '$estado', '$senha', NOW())";
+
+if($conexao->query($sql) === TRUE) {
+	$_SESSION['status_cadastro'] = true;
+}
+
+$conexao->close();
+
+header('Location: cadastro.php');
+exit;
+?>
